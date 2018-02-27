f9e09f294e4d58df58f50f3fbdc126f79ec5e552
Lennart Piro <l.piro@reply.de>
2018-02-27 16:56:08 +0100

    remove strides equal to default value

--- cnntest.py
+++ cnntest.py
@@ -66,12 +66,12 @@ if y_test is not None:
 #print(y_train)
 
 model = Sequential()
-model.add(Conv2D(64, kernel_size=(3, 3), strides=(1, 1),
+model.add(Conv2D(64, kernel_size=(3, 3),
                  padding='same',
                  activation='relu',
                  input_shape=input_shape))
 model.add(Dropout(0.2))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(Dropout(0.2))
 model.add(MaxPooling2D(pool_size=(2, 2)))
